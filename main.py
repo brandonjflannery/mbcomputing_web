03c89dfd926cff000d4053d72c8dcf1e9513d898
Brandon Flannery <brandonflannery@Brandons-MacBook-Pro.local>
2022-10-14 09:27:52 -0700

    Adding galaxy page as home

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-@app.route('/galaxy')
+@app.route('/')
 def galaxy():
 	return app.send_static_file('galaxy.html')
 
@@ -11,4 +11,3 @@ if __name__ == '__main__':
     app.run(threaded=True, port=5000)
 
 
-    
