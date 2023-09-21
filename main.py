75a84b5d85f4adb030d94d25248423b5ac346962
brandonjflannery <brandonflannery@brandons-mbp.lan>
2023-09-21 15:56:41 -0700

    Adding all changes for v2 of website

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-@app.route('/galaxy')
-def galaxy():
-	return app.send_static_file('galaxy.html')
+@app.route('/')
+def home():
+	return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
 
 
-    
