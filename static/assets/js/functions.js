27a49333490c43dd25edefca430a1f62eb2e2537
brandonjflannery <brandonflannery@brandons-mbp.lan>
2023-09-22 11:31:25 -0700

    Website update with slider and deployments improvements

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -253,6 +253,84 @@ $( document ).ready(function() {
 
   }
 
+  function workSlider2() {
+
+    $('.slider2--prev, .slider2--next').click(function() {
+
+      var $this = $(this),
+          curLeft = $('.slider2').find('.slider--item-left'),
+          curLeftPos = $('.slider2').children().index(curLeft),
+          curCenter = $('.slider2').find('.slider--item-center'),
+          curCenterPos = $('.slider2').children().index(curCenter),
+          curRight = $('.slider2').find('.slider--item-right'),
+          curRightPos = $('.slider2').children().index(curRight),
+          totalWorks = $('.slider2').children().length,
+          $left = $('.slider2--item-left'),
+          $center = $('.slider2--item-center'),
+          $right = $('.slider2--item-right'),
+          $item = $('.slider2--item');
+
+      $('.slider2').animate({ opacity : 0 }, 400);
+
+      setTimeout(function(){
+
+      if ($this.hasClass('slider2--next')) {
+        if (curLeftPos < totalWorks - 1 && curCenterPos < totalWorks - 1 && curRightPos < totalWorks - 1) {
+          $left.removeClass('slider2--item-left').next().addClass('slider2--item-left');
+          $center.removeClass('slider2--item-center').next().addClass('slider2--item-center');
+          $right.removeClass('slider--item-right').next().addClass('slider2--item-right');
+        }
+        else {
+          if (curLeftPos === totalWorks - 1) {
+            $item.removeClass('slider2--item-left').first().addClass('slider2--item-left');
+            $center.removeClass('slider2--item-center').next().addClass('slider2--item-center');
+            $right.removeClass('slider2--item-right').next().addClass('slider2--item-right');
+          }
+          else if (curCenterPos === totalWorks - 1) {
+            $left.removeClass('slider2--item-left').next().addClass('slider2--item-left');
+            $item.removeClass('slider2--item-center').first().addClass('slider2--item-center');
+            $right.removeClass('slider2--item-right').next().addClass('slider2--item-right');
+          }
+          else {
+            $left.removeClass('slider2--item-left').next().addClass('slider2--item-left');
+            $center.removeClass('slider2--item-center').next().addClass('slider2--item-center');
+            $item.removeClass('slider2--item-right').first().addClass('slider2--item-right');
+          }
+        }
+      }
+      else {
+        if (curLeftPos !== 0 && curCenterPos !== 0 && curRightPos !== 0) {
+          $left.removeClass('slider2--item-left').prev().addClass('slider2--item-left');
+          $center.removeClass('slider2--item-center').prev().addClass('slider2--item-center');
+          $right.removeClass('slider2--item-right').prev().addClass('slider2--item-right');
+        }
+        else {
+          if (curLeftPos === 0) {
+            $item.removeClass('slider2--item-left').last().addClass('slider2--item-left');
+            $center.removeClass('slider2--item-center').prev().addClass('slider2--item-center');
+            $right.removeClass('slider2--item-right').prev().addClass('slider2--item-right');
+          }
+          else if (curCenterPos === 0) {
+            $left.removeClass('slider2--item-left').prev().addClass('slider2--item-left');
+            $item.removeClass('slider2--item-center').last().addClass('slider2--item-center');
+            $right.removeClass('slider2--item-right').prev().addClass('slider2--item-right');
+          }
+          else {
+            $left.removeClass('slider2--item-left').prev().addClass('slider2--item-left');
+            $center.removeClass('slider2--item-center').prev().addClass('slider2--item-center');
+            $item.removeClass('slider2--item-right').last().addClass('slider2--item-right');
+          }
+        }
+      }
+
+    }, 400);
+
+    $('.slider2').animate({ opacity : 1 }, 400);
+
+    });
+
+  }
+
   function transitionLabels() {
 
     $('.work-request--information input').focusout(function(){
@@ -275,6 +353,7 @@ $( document ).ready(function() {
 
   outerNav();
   workSlider();
+  workSlider2();
   transitionLabels();
 
 });
